5271770af04e2ec6616ee079f9aaccf4c63dc3a9
Simon Vocella <voxsim@gmail.com>
2012-06-23 10:39:10 +0200

    fix dblp behaviour

--- dblp.py
+++ dblp.py
@@ -1,8 +1,8 @@
 import dryscrape
 
 # set up a web scraping session
-#sess = dryscrape.Session(base_url = 'http://www.informatik.uni-trier.de')
-sess = dryscrape.Session(base_url = 'http://www.dblp.org')
+sess = dryscrape.Session(base_url = 'http://www.informatik.uni-trier.de')
+#sess = dryscrape.Session(base_url = 'http://www.dblp.org')
 
 # there are some failing HTTP requests, so we need to enter
 # a more error-resistant mode (like real browsers do)
@@ -12,18 +12,18 @@ sess.set_error_tolerant(True)
 sess.set_attribute('auto_load_images', False)
 
 # visit homepage and search for a term
-#sess.visit('/~ley/db/indices/a-tree/index.html')
-sess.visit('/search/index.php')
+sess.visit('/~ley/db/indices/a-tree/index.html')
+#sess.visit('/search/index.php')
 
 # save a screenshot of the web page
 sess.render('dblp.png')
 print "Screenshot written to 'dblp.png'"
 
-#q = sess.at_xpath('//*[@name="author"]')
-q = sess.at_xpath('//*[@name="query"]')
+q = sess.at_xpath('//*[@name="author"]')
+#q = sess.at_xpath('//*[@name="query"]')
 search_term = 'Giuseppe Vizzari'
 q.set(search_term)
-#q.form().submit()
+q.form().submit()
 
 # extract all links
 for link in sess.xpath('//a[@href]'):
